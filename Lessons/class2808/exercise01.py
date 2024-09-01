99c04a6a5459d5615dce310424b1c868c20838e2
AnnieTran-IJ <2026.ha.anh.tran@uwcisak.jp>
2024-09-01 12:41:54 +0700

    Update exercise01.py

--- Lessons/class2808/exercise01.py
+++ Lessons/class2808/exercise01.py
@@ -1,1 +1,13 @@
+num_unsafe = input("Enter a number: ")
+#validate that the user a number. If not, it is a bad program!!
+for letter in num_unsafe:
+    if letter not in "0123456789":
+        print("Please enter a number.")
+        exit()
+num = int(num_unsafe)
+
+for i in range(2, num-1):
+    if num % i == 0:
+        print(f"{i} is the factor of {num}")
+
 
